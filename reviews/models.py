6d8611dbc8f269bed37d1d65e3f59251f32ceb25
Juhyeon Jeong <wwg6710@naver.com>
2024-05-21 21:31:04 +0900

    :sparkles: REVIEW,COMMENT CRUD 기본 틀 완성

--- reviews/models.py
+++ reviews/models.py
@@ -15,18 +15,16 @@ class Review(models.Model):
         ('4.5', 4.5),
         ('5', 5),
     )
-    store_id = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='review_store_id')
-    user_id = models.ForeignKey(Accounts, on_delete=models.CASCADE, related_name='review_user_id')
-    username = models.ForeignKey(Accounts, on_delete=models.CASCADE, related_name = 'review_username')
-    store_name = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='review_store_name')
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    user = models.ForeignKey(Accounts, on_delete=models.CASCADE)
     review_content = models.TextField()
     score = models.CharField(max_length=5, choices = SCORE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    review_id = models.ForeignKey(Review, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(Accounts, on_delete=models.CASCADE)
     comment_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
