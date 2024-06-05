ba49d3b53e4624b97259bf631bf58cb0706f74d2
jaeeunyun <yeonjaelee98@gmail.com>
2024-06-05 10:50:57 +0900

    :sparkles: 소셜 로그인 관련 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
+import requests
+from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+
+# from allauth.socialaccount.providers.kakao import views as kakao_view
 
 from .models import Bookmark
 from .serializers import (
@@ -13,6 +18,8 @@ from .serializers import (
     ProfileSerializer,
     BookmarkSerializer,
 )
+
+User = get_user_model()
 
 
 class SignupView(CreateAPIView):
@@ -62,3 +69,215 @@ class BookmarkViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # 북마크 생성 시, 자동으로 사용자 할당
         serializer.save(user=self.request.user)
+
+
+# 소셜 로그인
+
+
+class KakaoLogin(APIView):
+    def post(self, request):
+        client_id = KAKAO_REST_API_KEY
+
+        received_code = request.data.get("code")  # 받은 ?code='' 값
+        code_value = received_code.split("?code=")[-1]  # 코드 값만 추출
+        print(code_value)
+
+        kakao_token = requests.post(
+            "https://kauth.kakao.com/oauth/token",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            data={
+                "grant_type": "authorization_code",
+                "client_id": client_id,
+                "redirect_uri": f"{REDIRECT_URL}/redirect.html",
+                "code": code_value,
+            },
+        )
+        print(kakao_token.json)  # access_token 발급 완료
+
+        access_token = kakao_token.json()["access_token"]
+        refresh_token = kakao_token.json()["refresh_token"]
+
+        token_data = {"access": access_token, "refresh": refresh_token}
+        # access_token 으로 사용자 정보 가져오기
+        user_data = requests.get(
+            "https://kapi.kakao.com/v2/user/me",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        # 이메일, 닉네임, 프로필 사진 가져오기
+        user_data = user_data.json()
+
+        kakao_account = user_data.get("kakao_account")
+        user_email = kakao_account.get("email")
+        user_nickname = kakao_account.get("profile")["nickname"]
+        user_img = kakao_account.get("profile")["profile_image_url"]
+
+        try:
+            # 기존에 가입된 유저나 소셜 로그인 유저가 존재하면 로그인
+            user = User.objects.get(email=user_email)
+            social_user = SocialAccount.objects.filter(uid=user_email).first()
+
+            # 동일한 이메일의 유저가 있지만, 소셜 계정이 아닐 때
+            if social_user is None:
+                return Response(
+                    {"error": "소셜 계정이 아닌 이미 존재하는 이메일입니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # 소셜 계정이 카카오가 아닌 다른 소셜 계정으로 가입했을 때
+            if social_user.provider != "kakao":
+                return Response(
+                    {"error": "다른 소셜 계정으로 가입되어 있습니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # 소셜 로그인 사용자의 경우
+            if social_user:
+                # 사용자의 비밀번호 없이 로그인 가능한 JWT 토큰 생성
+                print(social_user)
+                refresh = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                        "provider": social_user.provider,
+                        "is_admin": user.is_admin,
+                        "msg": "로그인 성공",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+
+        except User.DoesNotExist:
+            # 기존에 가입된 유저가 없으면 유저 모델에 생성후 소셜어카운트에 포함시키는 로직
+            new_user = User.objects.create(
+                email=user_email,
+                nickname=user_nickname,
+                profile_img=user_img,
+            )
+
+            # 소셜 계정도 생성하고 포함시키기
+            SocialAccount.objects.create(
+                user_id=new_user.id,
+                uid=new_user.email,
+                provider="kakao",
+            )
+
+            # 새로운 사용자에 대한 JWT 토큰 생성
+            refresh = RefreshToken.for_user(new_user)
+
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    "is_admin": user.is_admin,
+                    "msg": "회원가입 성공",
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
+
+class GithubLogin(APIView):
+    def post(self, request):
+        client_id = SOCIAL_AUTH_GITHUB_CLIENT_ID
+        client_secret = SOCIAL_AUTH_GITHUB_SECRET
+
+        received_code = request.data.get("code")
+        code_value = received_code.split("?code=")[-1]
+
+        """토큰"""
+        github_token = requests.post(
+            f"https://github.com/login/oauth/access_token",
+            headers={"Accept": "application/json"},
+            data={
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "redirect_url": f"{REDIRECT_URL}/redirectGit.html",
+                "code": code_value,
+            },
+        )
+
+        access_token = github_token.json()["access_token"]
+
+        token_data = {"access": access_token, "auth": "github"}
+
+        """유저 데이터"""
+        user_data = requests.get(
+            "https://api.github.com/user",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/json",
+            },
+        )
+
+        user_data = user_data.json()
+
+        """유저 이메일"""
+        user_emails = requests.get(
+            "https://api.github.com/user/emails",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/json",
+            },
+        )
+        user_emails = user_emails.json()
+
+        try:
+            user = User.objects.get(email=user_emails[0]["email"])
+            print(user)
+            social_user = SocialAccount.objects.filter(
+                uid=user_emails[0]["email"]
+            ).first()
+
+            # if social_user:
+            #     refresh = RefreshToken.for_user(user)
+
+            #     return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "로그인 성공"}, status=status.HTTP_200_OK)
+
+            if social_user is None:
+                return Response(
+                    {"error": "소셜 계정이 아닌 이미 존재하는 이메일입니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            if social_user.provider != "github":
+                return Response(
+                    {"error": "다른 소셜 계정으로 가입되어 있습니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            if social_user:
+                refresh = RefreshToken.for_user(user)
+
+                return Response(
+                    {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                        "provider": social_user.provider,
+                        "is_admin": user.is_admin,
+                        "msg": "로그인 성공",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+
+        except User.DoesNotExist:
+            new_user = User.objects.create(
+                nickname=user_data.get("login"),
+                email=user_emails[0]["email"],
+                profile_img=user_data.get("avatar_url"),
+            )
+            SocialAccount.objects.create(
+                user_id=new_user.id,
+                uid=new_user.email,
+                provider="github",
+            )
+
+            refresh = RefreshToken.for_user(new_user)
+
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    "is_admin": user.is_admin,
+                    "msg": "회원가입 성공",
+                },
+                status=status.HTTP_201_CREATED,
+            )
--- local_gourmet/settings.py
+++ local_gourmet/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from datetime import timedelta
 
 import environ
@@ -25,8 +26,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # reading .env file
 environ.Env.read_env()
-SECRET_KEY = env('SECRET_KEY')
-OPENAI_API_KEY = env('OPENAI_API_KEY')
+SECRET_KEY = env("SECRET_KEY")
+OPENAI_API_KEY = env("OPENAI_API_KEY")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
@@ -39,22 +40,27 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Third Party
-    'rest_framework',
-    'corsheaders',
-    'rest_framework.authtoken',
-    'dj_rest_auth',
+    "rest_framework",
+    "corsheaders",
+    "rest_framework.authtoken",
+    "dj_rest_auth",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.kakao",
+    "allauth.socialaccount.providers.github",
     # local Apps
-    'reviews',
-    'accounts',
-    'stores',
-    'chatbots',
+    "reviews",
+    "accounts",
+    "stores",
+    "chatbots",
 ]
 
 AUTH_USER_MODEL = "accounts.Accounts"
@@ -65,29 +71,28 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000', 'http://127.0.0.1:3000'
-]
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-ROOT_URLCONF = 'local_gourmet.urls'
+ROOT_URLCONF = "local_gourmet.urls"
 
 TEMPLATES = [
     {
@@ -110,13 +115,13 @@ WSGI_APPLICATION = "local_gourmet.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 # sqlite 사용하고 싶을 시 위 DATABASES 코드 주석처리 후, docker-compose down하고
@@ -165,14 +170,14 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
-MEDIA_URL = "/media/"  #맨 앞에 /를 사용하여 절대경로로 지정
+MEDIA_URL = "/media/"  # 맨 앞에 /를 사용하여 절대경로로 지정
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SIMPLE_JWT = {
@@ -182,7 +187,7 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": env('SECRET_KEY'),
+    "SIGNING_KEY": env("SECRET_KEY"),
     "VERIFYING_KEY": "",
     "AUDIENCE": None,
     "ISSUER": None,
