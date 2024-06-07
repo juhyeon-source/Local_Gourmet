8aab9463d069acd1c4cc09fe876cae55ebcd961b
jaeeunyun <yeonjaelee98@gmail.com>
2024-06-07 19:36:03 +0900

    :sparkles: 소셜로그인 기능추가 (카카오, 구글)

--- local_gourmet/settings.py
+++ local_gourmet/settings.py
@@ -26,8 +26,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # reading .env file
 environ.Env.read_env()
+BASE_URL = env("BASE_URL")
 SECRET_KEY = env("SECRET_KEY")
 OPENAI_API_KEY = env("OPENAI_API_KEY")
+KAKAO_REST_API_KEY = env("KAKAO_REST_API_KEY")
+SOCIAL_AUTH_GOOGLE_CLIENT_ID = env("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
